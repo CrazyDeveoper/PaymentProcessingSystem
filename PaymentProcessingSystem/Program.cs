5d80204775167760389043805d55382e70c42c3f
AbdulKhalil <khalil.anisharaj@gmail.com>
2021-10-18 13:08:16 +0530

    referring project in unit test

--- PaymentProcessingSystem/Program.cs
+++ PaymentProcessingSystem/Program.cs
@@ -6,7 +6,7 @@ namespace PaymentProcessingSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, Welcome to Payemt Processing System!");
         }
     }
 }
