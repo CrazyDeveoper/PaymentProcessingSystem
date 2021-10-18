5c39842cd7306a90c2243f874c27ba4c7eabccbd
AbdulKhalil <khalil.anisharaj@gmail.com>
2021-10-18 13:05:06 +0530

    Add project files.

---
+++ PayementProcessingUnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PayementProcessingUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
