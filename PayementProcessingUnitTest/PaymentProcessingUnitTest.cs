38cf8963c625dffdf26918c76abd5afc69f7deba
crazydeveoper <khalil.anisharaj@gmail.com>
2021-10-19 13:14:53 +0530

    adding more test cases Test_GeneratePackingSlipForBookWithBookName Test_NonPhysicalProduct_VideoWithoutName

--- PayementProcessingUnitTest/PaymentProcessingUnitTest.cs
+++ PayementProcessingUnitTest/PaymentProcessingUnitTest.cs
@@ -18,8 +18,11 @@ namespace PayementProcessingUnitTest
             var result = PaymentProcessor.UserInputToProductType(new string[] { "video", "Learning to Ski" });
             Assert.AreEqual("Learning to Ski", result.ProductName);
             Assert.AreEqual("Packing slip generated for shipping.", result.ActionStatus[0]);
-            Assert.AreEqual(2, result.ActionStatus);
+            Assert.AreEqual("'First Aid' video added to the packing slip.", result.ActionStatus[1]);
+            
+            Assert.AreEqual(2, result.ActionStatus.Count);
         }
+        [TestMethod]
         public void Test_NonPhysicalProduct_VideoWithoutName()
         {
             //input will be 1. Product Type =video,membership,upgrade , 2. Product name which is nullable
@@ -27,7 +30,7 @@ namespace PayementProcessingUnitTest
             var result = PaymentProcessor.UserInputToProductType(new string[] { "video", "" });
             Assert.AreEqual("N/A", result.ProductName);
             Assert.AreEqual("Packing slip generated for shipping.", result.ActionStatus[0]);
-            Assert.AreEqual(2, result.ActionStatus);
+            Assert.AreEqual(1, result.ActionStatus.Count);
         }
 
         [TestMethod]
@@ -41,6 +44,18 @@ namespace PayementProcessingUnitTest
             Assert.AreEqual("Created a duplicate packing slip for the royalty department.", result.ActionStatus[2]);
             Assert.AreEqual(3, result.ActionStatus.Count);
         }
+        [TestMethod]
+        public void Test_GeneratePackingSlipForBookWithBookName()
+        {
+
+            var result = PaymentProcessor.UserInputToProductType(new string[] { "Book", "TDD Approach" });
+            Assert.AreEqual("TDD Approach", result.ProductName);
+            Assert.AreEqual("Packing slip generated for shipping.", result.ActionStatus[0]);
+            Assert.AreEqual("Added commission payment to the agent.", result.ActionStatus[1]);
+            Assert.AreEqual("Created a duplicate packing slip for the royalty department.", result.ActionStatus[2]);
+            Assert.AreEqual(3, result.ActionStatus.Count);
+        }
+
 
     }
 }
