ad4c245c4c6200a5f6524c22b07b7d7d8421449f
crazydeveoper <khalil.anisharaj@gmail.com>
2021-10-19 12:32:48 +0530

    another failed test case for physical product Test_GeneratePackingSlipForBook

--- PayementProcessingUnitTest/PaymentProcessingUnitTest.cs
+++ PayementProcessingUnitTest/PaymentProcessingUnitTest.cs
@@ -8,7 +8,7 @@ namespace PayementProcessingUnitTest
     {
         [TestMethod]
         public void Test_PhysicalProduct()
-        { 
+        {
         }
         [TestMethod]
         public void Test_NonPhysicalProduct_Video()
@@ -18,10 +18,20 @@ namespace PayementProcessingUnitTest
             var result = PaymentProcessor.UserInputToProductType(new string[] { "video", "Learning to Ski" });
             Assert.AreEqual("Learning to Ski", result.ProductName);
             Assert.AreEqual("Packing slip generated for shipping.", result.ActionStatus);
-       
+
         }
 
-        
+        [TestMethod]
+        public void Test_GeneratePackingSlipForBook()
+        {
+
+            var result = PaymentProcessor.UserInputToProductType(new string[] { "Book", "" });
+            Assert.AreEqual("N/A", result.ProductName);
+            Assert.AreEqual("Packing slip generated for shipping.", result.ActionStatus[0]);
+            Assert.AreEqual("Added commission payment to the agent.", result.ActionStatus[1]);
+            Assert.AreEqual("Created a duplicate packing slip for the royalty department.", result.ActionStatus[2]);
+            Assert.AreEqual(3, result.ActionStatus);
+        }
 
     }
 }
