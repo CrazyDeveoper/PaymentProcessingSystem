8057be5ca3b96dac77f837cf22fcd13505241302
crazydeveoper <khalil.anisharaj@gmail.com>
2021-10-19 11:38:56 +0530

    AddingUnitTest

---
+++ PayementProcessingUnitTest/PaymentProcessingUnitTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PayementProcessingSystem;
+
+namespace PayementProcessingUnitTest
+{
+    [TestClass]
+    public class PaymentProcessingUnitTest
+    {
+        [TestMethod]
+        public void Test_PhysicalProduct()
+        { 
+        }
+        [TestMethod]
+        public void Test_NonPhysicalProduct()
+        { 
+        }
+    }
+}
